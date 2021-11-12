c2f63771ce1fd96a840a23577eaef41fb6f6386d
Houston K <32914364+hu3man@users.noreply.github.com>
2021-11-11 16:45:59 -0800

    Deploy HueCoin

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const EthSwap = artifacts.require("EthSwap");
+const HuCoin = artifacts.require("HueCoin");
 
 module.exports = function(deployer) {
     deployer.deploy(EthSwap);
+    deployer.deploy(HueCoin);
 };
